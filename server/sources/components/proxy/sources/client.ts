927ce6b31c252583cd6df59764fad0fb319db661
Remi Deltombe <r.deltombe@talkwalker.com>
2020-01-15 12:34:03 +0100

    handle http request

--- server/sources/components/proxy/sources/client.ts
+++ server/sources/components/proxy/sources/client.ts
@@ -1,12 +1,70 @@
 
 import { Http } from '../../protocol/protocol'
 import * as https from 'https';
+import * as http from 'http';
 
 export class Client
 {
 	public send(request: Http.Request) : Promise<Http.Response>
 	{
-		return this.https(request);
+		return request.protocol === 'https' ? this.https(request) : this.http(request);
+	}
+
+	private http(request: Http.Request) : Promise<Http.Response>
+	{
+		return new Promise(resolve=>{
+			const headers : any = {};
+			const bannedHeader : string[] = ['host', 'accept-encoding', 'referer']
+
+			for(const header of request.header)
+			{
+				if(!bannedHeader.includes(header[0]))
+				{
+					//headers[header[0]] = header[1];
+				}
+			}
+			const req = http.request({
+			    hostname: request.hostname,
+			    port: request.port,
+			    path: request.path,
+			    method: request.method,
+			    headers: headers
+			}, function (res) {
+
+				const response = new Http.Response();
+				response.code = res.statusCode;
+
+				for(let i in res.headers)
+				{
+					if(Array.isArray(res.headers[i]))
+					{
+						response.header.set(i, (res.headers[i] as string[]).join(','));
+					}
+					else
+					{
+						response.header.set(i, res.headers[i] as string);
+					}
+				}
+
+				const buffer : any = [];
+				res.on('data',  (chunk) => {
+					buffer.push(chunk);
+				});
+
+				res.on('end',() => {
+			        response.rawBody = Buffer.concat(buffer);
+			        if(!response.isBinary())
+			        {
+						response.body = response.rawBody.toString();			        	
+			        }
+					resolve(response)
+				});
+			});
+
+			// write data to request body
+			req.write('');
+			req.end();
+		})
 	}
 
 	private https(request: Http.Request) : Promise<Http.Response>
