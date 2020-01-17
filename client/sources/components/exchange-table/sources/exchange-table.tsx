f8c28c5247089973030314cab73661b43c7f1647
Remi Deltombe <deltombe.remi@gmail.com>
2020-01-17 08:35:59 +0100

    create exchange-table component

---
+++ client/sources/components/exchange-table/sources/exchange-table.tsx
@@ -0,0 +1,153 @@
+import * as React from "react";
+
+import { Registration } from "event";
+import { Button, ButtonColor } from "button";
+import { InputText } from "input-text";
+import { Table, TableRowInterface } from "table";
+import { Link } from "link";
+import { Api, Endpoint } from "api";
+import { Proxy, Exchange } from "proxy";
+
+export interface ExchangeTableInterface {
+    exchanges: Exchange[];
+    onExchangeChange?: (exchange: Exchange) => void;
+}
+
+interface ExchangeTableRowInterface extends TableRowInterface {
+    exchange: Exchange;
+}
+
+export function ExchangeTable(config: ExchangeTableInterface) {
+    const { exchanges, onExchangeChange = () => {} } = config;
+
+    const [filterMethod, setFilterMethod] = React.useState<string>("");
+    const [filterRequest, setFilterRequest] = React.useState<string>("");
+
+    const regexp = [buildRegExp(filterMethod), buildRegExp(filterRequest)];
+    const rows = exchanges
+        .filter(
+            exchange =>
+                regexp[0].test(exchange.method) && regexp[1].test(exchange.url)
+        )
+        .map(exchangeToRow);
+
+    function buildRegExp(input: string): RegExp {
+        try {
+            return new RegExp(
+                input.length ? input.split("*").join(".*") : ".*"
+            );
+        } catch (e) {
+            return new RegExp(".*");
+        }
+    }
+
+    function exchangeToRow(exchange: Exchange): ExchangeTableRowInterface {
+        return {
+            exchange,
+            key: exchange.uuid,
+            items: [
+                { text: exchange.method },
+                {
+                    element: (
+                        <Link
+                            text={exchange.url}
+                            link={exchange.url}
+                            blank={true}
+                        />
+                    )
+                },
+                {
+                    element: (
+                        <div>
+                            <Button
+                                text={"Cache"}
+                                onClick={() => {
+                                    window.open(exchange.url, "_blank");
+                                }}
+                            />
+                            <Button
+                                text={exchange.cached ? "Disable" : "Enable"}
+                                color={
+                                    exchange.cached
+                                        ? ButtonColor.GREEN
+                                        : ButtonColor.RED
+                                }
+                                onClick={() => {
+                                    exchange.cached = !exchange.cached;
+                                    onExchangeChange(exchange);
+                                }}
+                            />
+                        </div>
+                    )
+                }
+            ]
+        };
+    }
+
+    return (
+        <div>
+            <Table
+                headers={[
+                    {
+                        key: "titles",
+                        items: [
+                            { text: "Method", width: "60px" },
+                            { text: "Request" },
+                            { text: "Cache", width: "200px" }
+                        ]
+                    },
+                    {
+                        key: "filters",
+                        items: [
+                            {
+                                element: (
+                                    <InputText
+                                        value={filterMethod}
+                                        onChange={v => setFilterMethod(v)}
+                                    />
+                                )
+                            },
+                            {
+                                element: (
+                                    <InputText
+                                        value={filterRequest}
+                                        onChange={v => setFilterRequest(v)}
+                                    />
+                                )
+                            },
+                            {
+                                element: (
+                                    <div>
+                                        <Button
+                                            text="Enable"
+                                            onClick={() =>
+                                                rows.forEach(row => {
+                                                    row.exchange.cached = true;
+                                                    onExchangeChange(
+                                                        row.exchange
+                                                    );
+                                                })
+                                            }
+                                        />
+                                        <Button
+                                            text="Disable"
+                                            onClick={() =>
+                                                rows.forEach(row => {
+                                                    row.exchange.cached = false;
+                                                    onExchangeChange(
+                                                        row.exchange
+                                                    );
+                                                })
+                                            }
+                                        />
+                                    </div>
+                                )
+                            }
+                        ]
+                    }
+                ]}
+                rows={rows}
+            />
+        </div>
+    );
+}
--- client/sources/components/proxy-detail/sources/proxy-detail.tsx
+++ client/sources/components/proxy-detail/sources/proxy-detail.tsx
@@ -9,6 +9,7 @@ import { Table, TableRowInterface } from "table";
 import { Link } from "link";
 import { Api, Endpoint } from "api";
 import { Proxy, Exchange } from "proxy";
+import { ExchangeTable } from "exchange-table";
 import { style } from "./styles";
 
 export interface ProxyDetailInterface {
@@ -22,72 +23,9 @@ interface ProxyDetailTableRowInterface extends TableRowInterface {
 }
 
 export function ProxyDetail(config: ProxyDetailInterface) {
-    const { proxy, exchanges, onExchangeChange = () => {} } = config;
-
-    const [filterMethod, setFilterMethod] = React.useState<string>("");
-    const [filterRequest, setFilterRequest] = React.useState<string>("");
+    const { proxy } = config;
 
     const link = `http://${proxy.hostname}:${proxy.port}`;
-    const regexp = [buildRegExp(filterMethod), buildRegExp(filterRequest)];
-    const rows = exchanges
-        .filter(
-            exchange =>
-                regexp[0].test(exchange.method) && regexp[1].test(exchange.url)
-        )
-        .map(exchangeToRow);
-
-    function buildRegExp(input: string): RegExp {
-        try {
-            return new RegExp(
-                input.length ? input.split("*").join(".*") : ".*"
-            );
-        } catch (e) {
-            return new RegExp(".*");
-        }
-    }
-
-    function exchangeToRow(exchange: Exchange): ProxyDetailTableRowInterface {
-        return {
-            exchange,
-            key: exchange.uuid,
-            items: [
-                { text: exchange.method },
-                {
-                    element: (
-                        <Link
-                            text={exchange.url}
-                            link={exchange.url}
-                            blank={true}
-                        />
-                    )
-                },
-                {
-                    element: (
-                        <div>
-                            <Button
-                                text={"Cache"}
-                                onClick={() => {
-                                    window.open(exchange.url, "_blank");
-                                }}
-                            />
-                            <Button
-                                text={exchange.cached ? "Disable" : "Enable"}
-                                color={
-                                    exchange.cached
-                                        ? ButtonColor.GREEN
-                                        : ButtonColor.RED
-                                }
-                                onClick={() => {
-                                    exchange.cached = !exchange.cached;
-                                    onExchangeChange(exchange);
-                                }}
-                            />
-                        </div>
-                    )
-                }
-            ]
-        };
-    }
 
     return (
         <div css={style}>
@@ -100,68 +38,7 @@ export function ProxyDetail(config: ProxyDetailInterface) {
                     <Link text={link} link={link} blank={true} />
                 </div>
             </div>
-            <Table
-                headers={[
-                    {
-                        key: "titles",
-                        items: [
-                            { text: "Method", width: "60px" },
-                            { text: "Request" },
-                            { text: "Cache", width: "200px" }
-                        ]
-                    },
-                    {
-                        key: "filters",
-                        items: [
-                            {
-                                element: (
-                                    <InputText
-                                        value={filterMethod}
-                                        onChange={v => setFilterMethod(v)}
-                                    />
-                                )
-                            },
-                            {
-                                element: (
-                                    <InputText
-                                        value={filterRequest}
-                                        onChange={v => setFilterRequest(v)}
-                                    />
-                                )
-                            },
-                            {
-                                element: (
-                                    <div>
-                                        <Button
-                                            text="Enable"
-                                            onClick={() =>
-                                                rows.forEach(row => {
-                                                    row.exchange.cached = true;
-                                                    onExchangeChange(
-                                                        row.exchange
-                                                    );
-                                                })
-                                            }
-                                        />
-                                        <Button
-                                            text="Disable"
-                                            onClick={() =>
-                                                rows.forEach(row => {
-                                                    row.exchange.cached = false;
-                                                    onExchangeChange(
-                                                        row.exchange
-                                                    );
-                                                })
-                                            }
-                                        />
-                                    </div>
-                                )
-                            }
-                        ]
-                    }
-                ]}
-                rows={rows}
-            />
+            <ExchangeTable {...config} />
         </div>
     );
 }
